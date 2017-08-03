3c7644a4b5adb5e74461196f01eecf67bff41ca6
Jesse Silber <jessesilber@gmail.com>
2017-08-03 11:10:10 -0400

    updated indiex with naming standard

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 "use strict";
 
-var PaymentRails_Recipient = require('./PaymentRails_Recipient');
-var PaymentRails_RecipientAccounts = require('./PaymentRails_RecipientAccounts');
-var PaymentRails_Balances = require('./PaymentRails_Balances');
-var PaymentRails_Payment = require('./PaymentRails_Payment');
-var PaymentRails_Batch = require('./PaymentRails_Batch');
+var Recipient = require('./Recipient');
+var RecipientAccounts = require('./RecipientAccounts');
+var Balances = require('./Balances');
+var Payment = require('./Payment');
+var Batch = require('./Batch');
 var Configuration = require('./Configuration');
 
 module.exports = {
   Configuration,
-  PaymentRails_Recipient,
-  PaymentRails_RecipientAccounts,
-  PaymentRails_Balances,
-  PaymentRails_Payment,
-  PaymentRails_Batch
+  Recipient,
+  RecipientAccounts,
+  Balances,
+  Payment,
+  Batch
 };
